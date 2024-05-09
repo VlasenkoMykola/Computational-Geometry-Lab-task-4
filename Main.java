5c50257538d1803e824b945eca94d4a92a9efe54
VlasenkoMykola <116251444+VlasenkoMykola@users.noreply.github.com>
2024-05-09 19:23:14 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -29,7 +29,7 @@ class Node {
 }
 
 class RangeTree {
-    Node root;
+    public Node root;
     public double minX, minY, maxX, maxY;
 
     public RangeTree(List<Point> points) {
@@ -100,17 +100,104 @@ class RangeTree {
 
 
 class DrawingPanel extends JPanel {
-    private List<Point> points;
+    private RangeTree rangeTree;
+    // BoundingBox for range query
     public double x1, y1, x2, y2;
+    // BoundingBox for the set of points
     private double minX, minY, maxX, maxY;
+    // coefficients from point coordinates to screen
+    public double coefficientX, coefficientY;
 
-    public DrawingPanel(List<Point> points, double minX, double minY, double maxX, double maxY) {
-        this.points = points;
-        this.minX = minX;
-        this.minY = minY;
-        this.maxX = maxX;
-        this.maxY = maxY;
-        setPreferredSize(new Dimension(600, 600));
+    public DrawingPanel(RangeTree rtree, double x1, double y1, double x2, double y2) {
+	this.rangeTree = rtree;
+	this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.minX = rangeTree.minX;
+        this.minY = rangeTree.minY;
+        this.maxX = rangeTree.maxX;
+        this.maxY = rangeTree.maxY;
+
+	int screenWidth = 600;
+	int screenHeight = 600;
+        setPreferredSize(new Dimension(screenWidth, screenHeight));
+	double pointsWidth = maxX - minX;
+	double pointsHeight = maxY - minY;
+	// to screen pixel coordinates
+	this.coefficientX = (double) screenWidth / pointsWidth;
+	this.coefficientY = (double) screenHeight/ pointsHeight;
+    }
+
+    public int panelX( double x) {
+	return (int) ((x - minX) * coefficientX);
+    }
+    public int panelY( double y) {
+        return (int) ((maxY - y) * coefficientY);
+    }
+
+
+
+    /**
+     * Draw an arrow line between two points.
+     * @param g the graphics component.
+     * @param x1 x-position of first point.
+     * @param y1 y-position of first point.
+     * @param x2 x-position of second point.
+     * @param y2 y-position of second point.
+     * @param d  the width of the arrow.
+     * @param h  the height of the arrow.
+     */
+    private void drawArrowLine(Graphics2D g, int x1, int y1, int x2, int y2, int d, int h) {
+	int dx = x2 - x1, dy = y2 - y1;
+	double D = Math.sqrt(dx*dx + dy*dy);
+	double xm = D - d, xn = xm, ym = h, yn = -h, x;
+	double sin = dy / D, cos = dx / D;
+
+	x = xm*cos - ym*sin + x1;
+	ym = xm*sin + ym*cos + y1;
+	xm = x;
+
+	x = xn*cos - yn*sin + x1;
+	yn = xn*sin + yn*cos + y1;
+	xn = x;
+
+	int[] xpoints = {x2, (int) xm, (int) xn};
+	int[] ypoints = {y2, (int) ym, (int) yn};
+
+	g.drawLine(x1, y1, x2, y2);
+	g.fillPolygon(xpoints, ypoints, 3);
+    }
+
+
+    private void drawNode(Graphics2D g2d, Node node) {
+        if (node == null)
+            return;
+
+        Point point = node.point;
+	g2d.setColor(Color.BLACK);
+	g2d.fillOval(panelX(point.x) - 3, panelY(point.y) - 3, 6, 6);
+
+	Node leftNode = node.left;
+	if (leftNode != null) {
+	    Point headpoint = leftNode.point;
+	    g2d.setColor(Color.GREEN);
+	    drawArrowLine(g2d,
+			  panelX(point.x), panelY(point.y),
+			  panelX(headpoint.x), panelY(headpoint.y),
+			  7, 7);
+	    drawNode(g2d, leftNode);
+	}
+	Node rightNode = node.right;
+	if (rightNode != null) {
+	    Point headpoint = rightNode.point;
+	    g2d.setColor(Color.YELLOW);
+	    drawArrowLine(g2d,
+			  panelX(point.x), panelY(point.y),
+			  panelX(headpoint.x), panelY(headpoint.y),
+			  7, 7);
+	    drawNode(g2d, rightNode);
+	}
     }
 
     @Override
@@ -118,43 +205,27 @@ class DrawingPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-	double pointsWidth = maxX - minX;
-	double pointsHeight = maxY - minY;
-
         // Draw points
-        for (Point point : points) {
-            int px = (int) ((point.x - minX) / pointsWidth * getWidth());
-            int py = (int) ((maxY - point.y) / pointsHeight * getHeight());
-            //int py = (int) ((point.y - minY) / pointsHeight * getHeight());
-            g2d.setColor(Color.BLACK);
-            g2d.fillOval(px - 3, py - 3, 6, 6);
-        }
+	drawNode(g2d, rangeTree.root);
 
 	float thickness = 3;
 	Stroke oldStroke = g2d.getStroke();
 	g2d.setStroke(new BasicStroke(thickness));
 
-        // Draw range query rectangle
-        //int rx1 = (int) ((x1 - minX) / pointsWidth * getWidth());
-        //int ry1 = (int) ((maxY - y1) / pointsHeight * getHeight());
-        //int rx2 = (int) ((x2 - minX) / pointsWidth * getWidth());
-        //int ry2 = (int) ((maxY - y2) / pointsHeight * getHeight());
-        int rx1 = (int) (((x1 - minX) / pointsWidth) * getWidth());
-	int rx2 = (int) (((x2 - minX) / pointsWidth) * getWidth());
-        int ry1 = (int) (((y1 - minY) / pointsHeight) * getHeight());
-	int ry2 = (int) (((y2 - minY) / pointsHeight) * getHeight());
-        g2d.setColor(Color.RED);
-        //g2d.drawRect(rx1, ry1, rx2 - rx1, ry2 - ry1);
-        g2d.drawRect(rx1, getHeight() - ry2, rx2 - rx1, ry2 - ry1);
-	//System.out.println("x1=" + x1 + " x2=" + x2 + " y1=" + y1 + " y2=" + y2);
-	//System.out.println("rx1=" + rx1 + " rx2=" + rx2 + " ry1=" + ry1 + " ry2=" + ry2);
-
         // Draw bounding box rectangle
         g2d.setColor(Color.BLUE);
         g2d.drawRect(1, 1, (int) getWidth()-2, getHeight()-2);
+        // Draw range query rectangle
+        int rx1 = panelX(x1);
+	int rx2 = panelX(x2);
+        int ry1 = panelY(y1);
+	int ry2 = panelY(y2);
+        g2d.setColor(Color.RED);
+        g2d.drawRect(rx1, ry2, rx2 - rx1, ry1 - ry2);
+	//System.out.println("x1=" + x1 + " x2=" + x2 + " y1=" + y1 + " y2=" + y2);
+	//System.out.println("rx1=" + rx1 + " rx2=" + rx2 + " ry1=" + ry1 + " ry2=" + ry2);
 
-        //g2d.setColor(Color.GREEN);
-        //g2d.drawRect(3, getHeight()-3-100, 100, 100);
+
 
 	g2d.setStroke(oldStroke);
     }
@@ -168,29 +239,25 @@ public class Main {
         // Example range query
         double x1 = 1.0, y1 = 1.0, x2 = 50.0, y2 = 50.0;
 
+        List<Point> result = rangeTree.queryRange(x1, y1, x2, y2);
+        System.out.println("Points within the range [" + x1 + ", " + y1 + "] to [" + x2 + ", " + y2 + "]:");
+        for (Point point : result) {
+            System.out.println("(" + point.x + ", " + point.y + ")");
+        }
+
         // Create Swing window
         JFrame frame = new JFrame("Range Tree Visualization");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 600);
 
         // Create drawing panel
-        DrawingPanel drawingPanel = new DrawingPanel(points, rangeTree.minX, rangeTree.minY, rangeTree.maxX, rangeTree.maxY);
-        drawingPanel.x1 = x1;
-        drawingPanel.y1 = y1;
-        drawingPanel.x2 = x2;
-        drawingPanel.y2 = y2;
+        DrawingPanel drawingPanel = new DrawingPanel(rangeTree, x1, y1, x2, y2);
 
         // Add drawing panel to the frame
         frame.add(drawingPanel);
 
         // Show the frame
         frame.setVisible(true);
-
-        List<Point> result = rangeTree.queryRange(x1, y1, x2, y2);
-        System.out.println("Points within the range [" + x1 + ", " + y1 + "] to [" + x2 + ", " + y2 + "]:");
-        for (Point point : result) {
-            System.out.println("(" + point.x + ", " + point.y + ")");
-        }
 
     }
 
